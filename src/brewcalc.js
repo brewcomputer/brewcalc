38af16d6874b421bce238b1e648d184c08bbd781
Yuriy Krutilin <y.krutilin@gmail.com>
2017-05-29 19:18:24 +0300

    IBU and water volumes attempt. Must to stop codind and do refactoring to prevent hell.

--- src/brewcalc.js
+++ src/brewcalc.js
@@ -14,6 +14,31 @@ const fermentableTypes = {
   adjunct: 'Adjunct',
 }
 
+//Hop USE May be "Boil", "Dry Hop", "Mash", "First Wort" or "Aroma".  Note that "Aroma" and "Dry Hop" do not contribute to the bitterness of the beer while the others do.
+//Aroma hops are added after the boil and do not contribute substantially to beer bitterness.
+const hopUse = {
+  boil: 'Boil',
+  dryHop: 'DryHop',
+  mash: 'Mash',
+  firstWort: 'FirstWort',
+  aroma: 'Aroma',
+}
+
+//Hop FORM May be "Pellet", "Plug" or "Leaf"
+const hopForms = {
+  pellet: 'Pellet',
+  plug: 'Plug',
+  leaf: 'Leaf',
+}
+
+//Mash type May be “Infusion”, “Temperature” or “Decoction” depending on the type of step.
+//Infusion denotes adding hot water, Temperature denotes heating with an outside heat source, and decoction denotes drawing off some mash for boiling.
+const mashType = {
+  infusion: 'Infusion',
+  temperature: 'Temperature',
+  decoction: 'Decoction',
+}
+
 const kilosToOunces = k => {
   return k * 35.2739619
 }
@@ -41,11 +66,15 @@ const options = () => {
   const slopeOfFGAA = -1.25
   //center Mash Temp for FG Adjust
   const centerMashTempForFGAA = 153.5
+  const grainAbsorb = 0.13
+  const mashTunDeadSpace = 3.03
   return {
     stepingEfficiency,
     ajustFGforMashTemperature,
     slopeOfFGAA,
     centerMashTempForFGAA,
+    grainAbsorb,
+    mashTunDeadSpace,
   }
 }
 
@@ -194,4 +223,148 @@ export const estimateFG = ({ recipe, batchSize }) => {
   if (batchSize > 0) totalFGp = 1.0 + totalFGp / batchSize / 1000.0
   else totalFGp = 1.0
   return totalFGp
+}
+
+const extractBoilGravity = recipe => {
+  let mGPs =
+    estimateOG({
+      recipe: recipe,
+      batchSize: recipe.batchSize,
+      afterBoil: true,
+      includeSugars: true,
+    }) - 1 //milliGPs
+  let avgBoilTime = 0
+  let t = 0
+
+  recipe.fermentables.map(fermentable => {
+    if (!fermentable.type == fermentableTypes.grain) {
+      t++
+      avgBoilTime += fermentable.time
+    }
+  })
+  if (t != 0) {
+    avgBoilTime = avgBoilTime / t
+  } else {
+    avgBoilTime = recipe.boilTime
+  }
+
+  return (
+    1 +
+    mGPs *
+      litersToGallons(recipe.batchSize) /
+      litersToGallons(recipe.boilSize) *
+      (avgBoilTime / recipe.boilTime)
+  )
+}
+
+const allGrainBoilGravity = recipe => {
+  let mGPs =
+    estimateOG({
+      recipe: recipe,
+      batchSize: recipe.batchSize,
+      afterBoil: true,
+      includeSugars: true,
+    }) - 1
+
+  return (
+    1 +
+    mGPs * litersToGallons(recipe.batchSize) / litersToGallons(recipe.boilSize)
+  )
+}
+
+export const getBoilGravity = recipe => {
+  if (recipe.type == recipeTypes.extract) {
+    return extractBoilGravity(recipe)
+  } else {
+    return allGrainBoilGravity(recipe)
+  }
+}
+
+const sum = array => array.reduce((pv, cv) => pv + cv, 0)
+
+export const totalGrains = recipe => {
+  return sum(
+    recipe.fermentables.map(
+      ({ amount, addAfterBoil }) => (!addAfterBoil ? amount : 0)
+    )
+  )
+}
+
+export const totalMashGrains = recipe => {
+  return sum(
+    recipe.fermentables.map(({ amount, type }) => {
+      if (type == fermentableTypes.grain) return amount
+      else return 0
+    })
+  )
+}
+
+export const totalMashWater = recipe => {
+  var totalWater = 0
+
+  if (recipe.mash.mashSteps == null || recipe.mash.mashSteps == 'undefined')
+    return 0
+
+  recipe.mash.mashSteps.map(mashStep => {
+    if (mashStep.type != mashType.decoction) {
+      totalWater += mashStep.infuseAmount
+    }
+  })
+
+  return totalWater + recipe.equipment.lauterDeadspace
+}
+
+export const vaterAvailableFromMash = recipe => {
+  return (
+    totalMashWater(recipe) - totalMashGrains(recipe) * options().grainAbsorb
+  )
+}
+
+export const spargeVol = recipe => {
+  const grainAbsLoss = options().grainAbsorb * totalMashGrains(recipe)
+
+  const vol =
+    recipe.equipment.boilSize +
+    grainAbsLoss -
+    recipe.equipment.topUpKettle +
+    options().mashTunDeadSpace -
+    totalMashWater(recipe)
+  return vol
+}
+
+export const calcWater = ({
+  batchSize,
+  totalGrains,
+  boilTime,
+  evapRate,
+  coolingLossPct,
+  mashTunDeadSpace = 0,
+  kettleDeadSpace = 0,
+  dissolvedVol = 0,
+  hopWaterLoss = 0,
+  pumpWaterLoss = 0,
+}) => {
+  const coolingLoss = batchSize * coolingLossPct
+
+  const grainAbsLoss = options().grainAbsorb * totalGrains
+
+  const boilOffLoss = evapRate * boilTime / 60
+
+  const intoFermenterVol = batchSize
+  const fromKettleVol = intoFermenterVol + pumpWaterLoss
+  const postBoilVol = fromKettleVol + hopWaterLoss + kettleDeadSpace
+  const hotPostBoilVol = postBoilVol + coolingLoss
+  const preBoilVol = hotPostBoilVol + boilOffLoss
+  const preBoilWaterVol = preBoilVol - dissolvedVol
+  const totalWater = preBoilWaterVol + grainAbsLoss + mashTunDeadSpace
+
+  return {
+    grainAbsLoss,
+    totalWater,
+    preBoilWaterVol,
+    preBoilVol,
+    hotPostBoilVol,
+    postBoilVol,
+    fromKettleVol,
+  }
 }
--- src/tests/brewcalc.test.js
+++ src/tests/brewcalc.test.js
@@ -1,4 +1,17 @@
-import { estimateOG, estimateFG } from '../brewcalc'
+import {
+  estimateOG,
+  estimateFG,
+  getBoilGravity,
+  bitterness,
+  calcWater,
+  totalGrains,
+  totalMashGrains,
+  totalMashWater,
+  vaterAvailableFromMash,
+  mashVolumeNeeded,
+  spargeVol,
+  calcIBU,
+} from '../brewcalc'
 import { recipe as AussieAle } from './data/recipe/AussieAle.js'
 import { recipe as MiddyPig } from './data/recipe/Middy Pig.js'
 
@@ -60,4 +73,77 @@ test('estimateFG', () => {
       batchSize: 0.0,
     })
   ).toBeCloseTo(1, 2)
+})
+
+test('totalGrains', () => {
+  expect(totalGrains(MiddyPig).toFixed(2)).toBe('4.35')
+  expect(totalGrains(AussieAle).toFixed(2)).toBe('5.53')
+})
+
+test('totalMashGrains', () => {
+  expect(totalMashGrains(MiddyPig).toFixed(2)).toBe('0.68')
+  expect(totalMashGrains(AussieAle).toFixed(2)).toBe('5.26')
+})
+
+test('totalMashWater', () => {
+  expect(totalMashWater(MiddyPig)).toBe(0)
+  expect(totalMashWater(AussieAle).toFixed(2)).toBe('24.45')
+})
+
+test('vaterAvailableFromMash', () => {
+  //23.79
+  expect(vaterAvailableFromMash(AussieAle).toFixed(2)).toBe('23.76')
+})
+
+test('spargeVol', () => {
+  //16.36
+  expect(spargeVol(AussieAle).toFixed(2)).toBe('16.39')
+})
+
+test('calcWaterMiddyPig', () => {
+  const calcResults = calcWater({
+    batchSize: MiddyPig.equipment.batchSize,
+    totalGrains: totalMashGrains(MiddyPig),
+    boilTime: MiddyPig.equipment.batchSize,
+    evapRate: MiddyPig.equipment.evapRate,
+    coolingLossPct: MiddyPig.equipment.coolingLossPct,
+  })
+
+  expect(calcResults).toBeDefined()
+  expect(calcResults.grainAbsLoss.toFixed(2)).toBe('0.09')
+
+  //expect(calcResults.totalWater).toBeCloseTo(13.635, 3)
+
+  //expect(calcResults.preBoilWaterVol).toBeCloseTo(11.375, 3)
+
+  //expect(calcResults.postBoilVol).toBeCloseTo(10.2, 3)
+
+  //expect(calcResults.hotPostBoilVol).toBeCloseTo(10.625, 3)
+})
+
+test('calcWaterAussieAle', () => {
+  const calcResults = calcWater({
+    batchSize: AussieAle.equipment.batchSize,
+    totalGrains: totalMashGrains(AussieAle),
+    boilTime: AussieAle.equipment.batchSize,
+    evapRate: AussieAle.equipment.evapRate,
+    coolingLossPct: AussieAle.equipment.coolingLossPct,
+  })
+
+  expect(calcResults).toBeDefined()
+
+  expect(calcResults.grainAbsLoss.toFixed(2)).toBe('0.68')
+
+  //expect(calcResults.totalWater).toBeCloseTo(13.635, 3)
+
+  //expect(calcResults.preBoilWaterVol).toBeCloseTo(11.375, 3)
+
+  //expect(calcResults.postBoilVol).toBeCloseTo(10.2, 3)
+
+  //expect(calcResults.hotPostBoilVol).toBeCloseTo(10.625, 3)
+})
+
+test('boilGravity', () => {
+  expect(getBoilGravity(AussieAle)).toBeCloseTo(1.031, 2)
+  expect(getBoilGravity(MiddyPig)).toBeCloseTo(1.084, 2)
 })
