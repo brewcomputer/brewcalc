4db7ef4e1dcd17222ac3aa032ce4f7c35daf3ba8
Yuriy Krutilin <y.krutilin@gmail.com>
2017-06-19 16:21:48 +0300

    Rager and Tinseth IBU calcs

--- src/hops.js
+++ src/hops.js
@@ -1,6 +1,7 @@
 // @flow
 import type { Recipe } from './types/recipe'
 import { sum, kgToOunces, litersToGallons } from './utils.js'
+import type { Hop } from './types/hop'
 import { HopForms } from './types/hop'
 
 const ibuUtilization = (
@@ -18,7 +19,7 @@ const ibuUtilization = (
 //IBU = (U * ozs hops * 7490)/Volume (in gallons) U represents the utilization of the hops (conversion to iso-alpha-acids) based on boil time and wort gravity.
 //U = bigness factor * boil time factor
 
-export const bitternessIBU = (
+export const bitternessIbuTinseth = (
   { hops }: Recipe,
   avgBoilGravityPts: number,
   postBoilVolume: number
@@ -41,3 +42,72 @@ export const bitternessIBU = (
 //The preceived bitterness expressed in a ratio of IBUs to gravity. This is frequently seen expressed as BU/GU.
 //The Gravity Units are the decimal portion of the original gravity
 export const bitternessRatio = (ibu: number, gu: number) => ibu / gu
+
+//rager
+const ragerHopGravityAdjustment = sgb => sgb <= 1.050 ? 0 : (sgb - 1.050) / 0.2
+const ragerHopUtil = time => {
+  if (time == 0) {
+    return 0.0
+  } else if (time <= 5) {
+    return 0.05
+  } else if (time <= 10) {
+    return 0.06
+  } else if (time <= 15) {
+    return 0.08
+  } else if (time <= 20) {
+    return 0.101
+  } else if (time <= 25) {
+    return 0.121
+  } else if (time <= 30) {
+    return 0.153
+  } else if (time <= 35) {
+    return 0.188
+  } else if (time <= 40) {
+    return 0.228
+  } else if (time <= 45) {
+    return 0.269
+  } else if (time <= 50) {
+    return 0.281
+  } else if (time <= 55) {
+    return 0.291
+  } else {
+    return 0.30
+  }
+}
+
+const ragerHopIbuFromWeight = (util, alpha, wt, vol, ga, wtFactor) =>
+  util * alpha * wt * wtFactor / (vol * (1.0 + ga))
+
+export const ragerHopIbu = (
+  amount: number,
+  alpha: number,
+  time: number,
+  sg: number,
+  vol: number
+) =>
+  time <= 0.0 || amount <= 0.0 || alpha < 0.0
+    ? 0
+    : ragerHopIbuFromWeight(
+        ragerHopUtil(Math.floor(time + 0.5)),
+        alpha,
+        amount,
+        vol,
+        ragerHopGravityAdjustment(sg),
+        100.0 / 1.34
+      )
+
+export const bitternessIbuRager = (
+  { hops }: Recipe,
+  avgBoilGravityPts: number,
+  postBoilVolume: number
+) =>
+  sum(
+    hops.map(({ amount, alpha, time }: Hop) =>
+      ragerHopIbu(
+        kgToOunces(amount),
+        alpha * 100,
+        time,
+        avgBoilGravityPts,
+        litersToGallons(postBoilVolume)
+      ))
+  )
--- src/tests/hops.test.js
+++ src/tests/hops.test.js
@@ -1,13 +1,17 @@
 // @flow
 declare var test: any;
 declare var expect: any;
-import { bitternessIBU, bitternessRatio } from '../hops'
+import {
+  bitternessIbuTinseth,
+  bitternessRatio,
+  bitternessIbuRager
+} from '../hops'
 import { originalGravity, gravityPoints, finalGravity } from '../brewcalc'
 import { calculateVolumes } from '../volumes'
 import { recipe as AussieAle } from './data/AussieAle.js'
 import { equipment as AussieAleEquipment } from './data/AussieAle.js'
 
-test('bitternessIBU', () => {
+test('bitternessIbuTinseth', () => {
   const ogPts = originalGravity(
     AussieAle.batchSize,
     gravityPoints(AussieAle, AussieAleEquipment)
@@ -24,15 +28,13 @@ test('bitternessIBU', () => {
 
   const avgBoilGravityPts = (ogPts + fgPts) / 2
 
-  const postBoilVolume = calculateVolumes(
-    AussieAle,
-    AussieAleEquipment
-  ).postBoilVolume
-
   expect(
-    bitternessIBU(AussieAle, avgBoilGravityPts, postBoilVolume)
-    //28.0  according BeerSmith
-  ).toBeCloseTo(27.21, 2)
+    bitternessIbuTinseth(
+      AussieAle,
+      avgBoilGravityPts,
+      AussieAleEquipment.batchSize + AussieAleEquipment.trubChillerLoss
+    )
+  ).toBeCloseTo(28, 0)
 })
 
 test('bitternessRatio', () => {
@@ -52,14 +54,39 @@ test('bitternessRatio', () => {
 
   const avgBoilGravityPts = (ogPts + fgPts) / 2
 
-  const postBoilVolume = calculateVolumes(
+  const ibu = bitternessIbuTinseth(
     AussieAle,
-    AussieAleEquipment
-  ).postBoilVolume
-
-  const ibu = bitternessIBU(AussieAle, avgBoilGravityPts, postBoilVolume)
+    avgBoilGravityPts,
+    AussieAleEquipment.batchSize + AussieAleEquipment.trubChillerLoss
+  )
 
   const gu = ogPts * 1000
-  //0.636  according BeerSmith
-  expect(bitternessRatio(ibu, gu)).toBeCloseTo(0.618, 3)
+  expect(bitternessRatio(ibu, gu)).toBeCloseTo(0.64, 0)
+})
+
+test('bitternessIbuRager', () => {
+  const ogPts = originalGravity(
+    AussieAle.batchSize,
+    gravityPoints(AussieAle, AussieAleEquipment)
+  ) - 1
+
+  const fgPts = finalGravity(
+    AussieAle.batchSize,
+    gravityPoints(
+      AussieAle,
+      AussieAleEquipment,
+      AussieAle.yeasts[0].attenuation
+    )
+  ) - 1
+
+  const avgBoilGravityPts = (ogPts + fgPts) / 2
+
+  expect(
+    bitternessIbuRager(
+      AussieAle,
+      avgBoilGravityPts,
+      AussieAleEquipment.batchSize + AussieAleEquipment.trubChillerLoss
+    )
+  ).toBeCloseTo(21, 0)
+  //22.2 by beerSmith, I suppose that there is additional ajustments depends of the Hop form
 })
