0bcd9bc3527bbbaa448facfe7ac92c287efad4f9
Yuriy Krutilin <y.krutilin@gmail.com>
2017-07-18 16:14:07 +0300

    Upload recipe data from beerxml

--- src/components/RecipeUploader.js
+++ src/components/RecipeUploader.js
@@ -1,31 +1,42 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import Recipe from './Recipe'
 import { Grid, FormControl, FormGroup } from 'react-bootstrap'
 import { importFromBeerXml } from '../lib/importFromBeerXml'
 
-import { recipe, equipment } from '../lib/tests/data/AussieAle.js'
+let recipe = undefined
+let equipment = undefined
 
-const RecipeUploader = () => (
-    <Grid>
-        <FormGroup>
-            <FormControl
-                id="formControlsFile"
-                type="file"
-                label="File"
-                onChange={(e) => {
-                    const reader = new FileReader()
-                    reader.readAsText(e.target.files[0])
-                    reader.onloadend = function () {
-                        console.log(importFromBeerXml(reader.result).recipe)
-                    }
-                }}
-            />
-        </FormGroup>
-        Upload recipe in the BeerXml format
+const RecipeUploader = () => {
 
-        {recipe !== undefined &&
-            <Recipe recipe={recipe} equipment={equipment} />
-        }
-    </Grid >
-)
+    const recipeContainer = <Recipe recipe={recipe} equipment={equipment} />
+
+    return (
+        <Grid>
+            <FormGroup>
+                <FormControl
+                    id="formControlsFile"
+                    type="file"
+                    label="File"
+                    onChange={(e) => {
+                        const reader = new FileReader()
+                        reader.readAsText(e.target.files[0])
+                        reader.onloadend = function () {
+                            recipe = importFromBeerXml(reader.result).recipe
+                            equipment = importFromBeerXml(reader.result).equipment
+
+                            ReactDOM.render(
+                                <Recipe recipe={recipe} equipment={equipment} />,
+                                document.getElementById('root')
+                            )
+                        }
+                    }}
+                />
+            </FormGroup>
+            Upload recipe in the BeerXml format
+
+        {recipe !== undefined && recipeContainer}
+        </Grid >
+    )
+}
 export default RecipeUploader
