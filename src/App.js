85e9fe3792c4d3077eb6fcc669ab43d996bb3355
Yuriy Krutilin <y.krutilin@gmail.com>
2017-08-02 22:59:34 +0300

    Add redux minimize UI

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
 import React from 'react'
 import { Editor, EditorState, ContentState } from 'draft-js'
-import {
-  FormGroup,
-  FormControl,
-  Grid,
-  Row,
-  Col,
-  Nav,
-  NavItem,
-  Panel
-} from 'react-bootstrap'
+import { FormGroup, FormControl, Grid, Row, Col, Panel } from 'react-bootstrap'
 
 import { recipeOne as recipe } from './lib/tests/data/GenericOneHF.js'
 import { equipment } from './lib/tests/data/Pot50L.js'
@@ -19,97 +10,109 @@ import { importFromBeerXml } from './lib/importFromBeerXml'
 import Stats from './components/Stats'
 import StatsWater from './components/StatsWater'
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
+import { Provider, connect } from 'react-redux'
+import { createStore } from 'redux'
 
-    this.state = {
-      selectedKey: 'recipe',
-      equipment: equipment,
-      recipe: recipe,
-      editorState: EditorState.createWithContent(
-        ContentState.createFromText(
-          JSON.stringify(recipe, null, 4)
+const defaultState = {
+  editorState: EditorState.createWithContent(
+    ContentState.createFromText(JSON.stringify({ recipe, equipment }, null, 4))
+  )
+}
+const reducer = (state = defaultState, { payload, type }) => {
+  if (type === 'UPDATE_EDITOR_STATE') {
+    return {
+      ...state,
+      editorState: payload
+    }
+  }
+  return state
+}
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+const JsonEditor = ({ editorState, onSaveEditorState }) => {
+  const tryParse = editorState => {
+    try {
+      return JSON.parse(editorState.getCurrentContent().getPlainText())
+    } catch (error) {
+      return {
+        catchError: true,
+        error: error
+      }
+    }
+  }
+
+  const onXmlLoaded = e => {
+    const reader = new FileReader()
+    reader.readAsText(e.target.files[0])
+    reader.onloadend = function() {
+      const result = importFromBeerXml(reader.result)
+      onSaveEditorState(
+        EditorState.createWithContent(
+          ContentState.createFromText(
+            JSON.stringify(
+              { recipe: result.recipe, equipment: result.equipment },
+              null,
+              4
+            )
+          )
         )
       )
     }
-
-    this.onChange = editorState => {
-      try {
-        const currentJSON = JSON.parse(
-          this.state.editorState.getCurrentContent().getPlainText()
-        )
-        this.state.selectedKey === 'recipe' ? this.setState({ editorState, recipe: currentJSON }) : this.setState({ editorState, equipment: currentJSON })
-
-      } catch (error) {
-        this.setState({ editorState: EditorState.undo(this.state.editorState) })
-      }
-    }
-
-    this.onXmlLoaded = e => {
-      const reader = new FileReader()
-      reader.readAsText(e.target.files[0])
-      const self = this
-      reader.onloadend = function () {
-        const result = importFromBeerXml(reader.result)
-        self.setState({ equipment: result.equipment, recipe: result.recipe })
-        self.setState({
-          editorState: EditorState.createWithContent(
-            ContentState.createFromText(
-              JSON.stringify(self.state[self.state.selectedKey], null, 4)
-            )
-          )
-        })
-      }
-    }
-
-    this.handleSelect = (selectedKey) => {
-      this.setState({
-        selectedKey,
-        editorState: EditorState.createWithContent(
-          ContentState.createFromText(
-            JSON.stringify(this.state[selectedKey], null, 4)
-          )
-        )
-      })
-    }
   }
+  return (
+    <Grid>
+      <Panel header="Upload from BeerXML">
+        <FormGroup>
+          <FormControl
+            id="formControlsFile"
+            type="file"
+            label="File"
+            onChange={onXmlLoaded}
+          />
+        </FormGroup>
+      </Panel>
+      <Row className="show-grid">
+        <Col md={6}>
+          <Editor editorState={editorState} onChange={onSaveEditorState} />
+        </Col>
+        {tryParse(editorState).catchError !== true
+          ? <Col md={6}>
+              <Stats {...tryParse(editorState)} />
+              <StatsWater {...tryParse(editorState)} />
+            </Col>
+          : <Col md={6}>
+              <Panel header={tryParse(editorState).error.toString()} />
+            </Col>}
+      </Row>
+    </Grid>
+  )
+}
+
+const mapStateToProps = ({ editorState }) => ({ editorState })
+
+const mapDispatchToProps = dispatch => ({
+  onSaveEditorState: editorState => {
+    dispatch({
+      type: 'UPDATE_EDITOR_STATE',
+      payload: editorState
+    })
+  }
+})
+
+const JsonRecipeCalc = connect(mapStateToProps, mapDispatchToProps)(JsonEditor)
+
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <Grid>
-          <Row className="show-grid">
-            <Panel header="Upload from BeerXML">
-              <FormGroup>
-                <FormControl
-                  id="formControlsFile"
-                  type="file"
-                  label="File"
-                  onChange={this.onXmlLoaded}
-                />
-              </FormGroup>
-            </Panel >
-
-            <Nav bsStyle="pills" activeKey={1} onSelect={this.handleSelect}>
-              <NavItem eventKey={'recipe'}>Recipe</NavItem>
-              <NavItem eventKey={'equipment'} title="Item">Equipment</NavItem>
-            </Nav>
-            <Col md={6}>
-              <Editor
-                editorState={this.state.editorState}
-                onChange={this.onChange}
-              />
-            </Col>
-            <Col md={6}>
-              <Stats recipe={this.state.recipe} equipment={this.state.equipment} />
-              <StatsWater
-                recipe={this.state.recipe}
-                equipment={this.state.equipment}
-              />
-            </Col>
-          </Row>
-        </Grid>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <JsonRecipeCalc />
+        </div>
+      </Provider>
     )
   }
 }
