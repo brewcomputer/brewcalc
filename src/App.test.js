5f34693de012a841b80d48c48466f4a4ce443143
Yuriy Krutilin <y.krutilin@gmail.com>
2017-08-21 14:24:27 +0300

    disable Ace warnings

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
+console.warn = jest.genMockFunction()
+
 it('renders without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(<App />, div)
