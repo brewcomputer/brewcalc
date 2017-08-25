7a3d846c765f0affc2b507451bcf869d36bdc3a9
Yuriy Krutilin <y.krutilin@gmail.com>
2017-08-25 17:13:36 +0300

    fix tests

---
+++ app/src/lib/data/Pot50L.js
@@ -0,0 +1,22 @@
+// @flow
+import type { Equipment } from '../../../node_modules/brewcalc/lib/brewcalc.min.js'
+
+export const equipment: Equipment = {
+  name: 'Pot (13 Gal/50 L) - BIAB',
+  boilSize: 36.0199780,
+  batchSize: 23.0154363,
+  tunVolume: 50.0055779,
+  tunWeight: 6.9989246,
+  tunSpecificHeat: 0.1200000,
+  topUpWater: '0.0000000',
+  trubChillerLoss: 3.8232879,
+  evapRate: 0.149231510,
+  boilTime: '90.0000000',
+  calcBoilVolume: 'TRUE',
+  lauterDeadspace: 0,
+  topUpKettle: 0,
+  hopUtilization: '100.0000000',
+  coolingLossPct: 0.04,
+  efficiency: 0.68,
+  BIAB: false
+}
