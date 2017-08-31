e44536e9796364641805142cb85be2aca0b4adef
Yuriy Krutilin <y.krutilin@gmail.com>
2017-08-31 10:48:23 +0300

    Equipment optional fields

--- src/types/equipment.js
+++ src/types/equipment.js
@@ -3,13 +3,13 @@ export type Equipment = {
   name: string,
   batchSize: number,
   boilSize: number,
-  tunWeight: number,
-  tunVolume: number,
-  tunSpecificHeat: number,
-  evapRate: number,
-  coolingLossPct: number,
-  trubChillerLoss: number,
-  lauterDeadspace: number,
-  topUpKettle: number,
-  BIAB: boolean
+  tunWeight?: number,
+  tunVolume?: number,
+  tunSpecificHeat?: number,
+  evapRate?: number,
+  coolingLossPct?: number,
+  trubChillerLoss?: number,
+  lauterDeadspace?: number,
+  topUpKettle?: number,
+  BIAB?: boolean
 }
