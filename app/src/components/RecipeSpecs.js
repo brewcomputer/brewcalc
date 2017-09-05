2ed444c01d0c825b8d115224dcb0ffa8e9c92879
Yuriy Krutilin <y.krutilin@gmail.com>
2017-09-05 14:59:19 +0300

    readable names in Recipe Specs

--- app/src/components/RecipeSpecs.js
+++ app/src/components/RecipeSpecs.js
@@ -11,9 +11,9 @@ const RecipeSpecs = ({ recipe, equipment }) => {
             <li><b>Name: </b>{name}</li>
             <li><b>Brewer: </b>{brewer}</li>
             <li><b>Type: </b>{type}</li>
-            <li><b>BatchSize: </b>{batchSize.toFixed(2)} L</li>
-            <li><b>BoilSize: </b>{boilSize.toFixed(2)} L</li>
-            <li><b>BoilTime: </b>{boilTime.toFixed(2)} min</li>
+            <li><b>Batch Size: </b>{batchSize.toFixed(2)} L</li>
+            <li><b>Boil Size: </b>{boilSize.toFixed(2)} L</li>
+            <li><b>Boil Time: </b>{boilTime.toFixed(2)} min</li>
           </ul>
         </Col>
         <Col md={6}>
@@ -24,18 +24,18 @@ const RecipeSpecs = ({ recipe, equipment }) => {
           ) : (
               <ul>
                 <li><b>Equipment Name: </b>{equipment.name}</li>
-                <li><b>BatchSize: </b>{equipment.batchSize.toFixed(2)} L</li>
-                <li><b>BoilSize: </b>{equipment.boilSize.toFixed(2)} L</li>
+                <li><b>Batch Size: </b>{equipment.batchSize.toFixed(2)} L</li>
+                <li><b>Boil Size: </b>{equipment.boilSize.toFixed(2)} L</li>
                 <li><b>Efficiency: </b>{equipment.efficiency}</li>
-                <li><b>EvapRate: </b>{equipment.evapRate.toFixed(2)}</li>
-                <li><b>CoolingLossPct: </b>{equipment.coolingLossPct}</li>
+                <li><b>Evap Rate: </b>{equipment.evapRate.toFixed(2)}</li>
+                <li><b>Cooling Loss: </b>{equipment.coolingLossPct * 100} %</li>
                 {equipment.trubChillerLoss > 0 &&
                   <li>
-                    <b>TrubChillerLoss: </b>{equipment.trubChillerLoss.toFixed(2)} L
+                    <b>Trub Chiller Loss: </b>{equipment.trubChillerLoss.toFixed(2)} L
             </li>}
                 {equipment.lauterDeadspace > 0 &&
                   <li>
-                    <b>LauterDeadspace: </b>{equipment.lauterDeadspace.toFixed(2)} L
+                    <b>Lauter Deadspace: </b>{equipment.lauterDeadspace.toFixed(2)} L
             </li>}
                 {equipment.topUpKettle > 0 &&
                   <li><b>TopUpKettle: </b>{equipment.topUpKettle.toFixed(2)} L</li>}
