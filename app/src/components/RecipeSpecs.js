b95092e79a4e42b0ffb9a0825699c42f71f6c8b3
Yuriy Krutilin <y.krutilin@gmail.com>
2017-09-06 15:14:51 +0300

    Detailed descriptions for specs

--- app/src/components/RecipeSpecs.js
+++ app/src/components/RecipeSpecs.js
@@ -8,12 +8,13 @@ const RecipeSpecs = ({ recipe, equipment }) => {
       <Row className="show-grid">
         <Col md={6}>
           <ul>
-            <li><b>Name: </b>{name}</li>
-            <li><b>Brewer: </b>{brewer}</li>
-            <li><b>Type: </b>{type}</li>
-            <li><b>Batch Size: </b>{batchSize.toFixed(2)} L</li>
-            <li><b>Boil Size: </b>{boilSize.toFixed(2)} L</li>
-            <li><b>Boil Time: </b>{boilTime.toFixed(2)} min</li>
+            <li title="Name of the recipe"><b>Name: </b>{name}</li>
+            <li title="Name of the brewer"><b>Brewer: </b>{brewer}</li>
+            <li title="May be one of “Extract”, “Partial Mash” or “All Grain”"><b>Type: </b>{type}</li>
+            <li title="Target size of the finished batch in liters"><b>Batch Size: </b>{batchSize.toFixed(2)} L</li>
+            <li title="Starting size for the main boil of the wort in liters"><b>Boil Size: </b>{boilSize.toFixed(2)} L</li>
+            <li title="The total time to boil the wort in minutes"><b>Boil Time: </b>{boilTime.toFixed(2)} min</li>
+            <li title="The percent brewhouse efficiency to be used for estimating the starting gravity of the beer"><b>Efficiency: </b>{equipment.efficiency}</li>
           </ul>
         </Col>
         <Col md={6}>
@@ -23,24 +24,23 @@ const RecipeSpecs = ({ recipe, equipment }) => {
             </ul>
           ) : (
               <ul>
-                <li><b>Equipment Name: </b>{equipment.name}</li>
-                <li><b>Batch Size: </b>{equipment.batchSize.toFixed(2)} L</li>
-                <li><b>Boil Size: </b>{equipment.boilSize.toFixed(2)} L</li>
-                <li><b>Efficiency: </b>{equipment.efficiency}</li>
-                <li><b>Evap Rate: </b>{equipment.evapRate.toFixed(2)}</li>
-                <li><b>Cooling Loss: </b>{equipment.coolingLossPct * 100} %</li>
+                <li title="Name of the equipment profile – usually a text description of the brewing setup"><b>Equipment Name: </b>{equipment.name}</li>
+                <li title="The target volume of the batch at the start of fermentation"><b>Batch Size: </b>{equipment.batchSize.toFixed(2)} L</li>
+                <li title="The pre-boil volume used in this particular instance for this equipment setup.  Note that this may be a calculated"><b>Boil Size: </b>{equipment.boilSize.toFixed(2)} L</li>
+                <li title="The percentage of wort lost to evaporation per hour of the boil"><b>Evap Rate: </b>{equipment.evapRate.toFixed(2)} % per hour</li>
+                <li title=""><b>Cooling Loss: </b>{equipment.coolingLossPct * 100} %</li>
                 {equipment.trubChillerLoss > 0 &&
-                  <li>
+                  <li title="The amount of wort normally lost during transition from the boiler to the fermentation vessel">
                     <b>Trub Chiller Loss: </b>{equipment.trubChillerLoss.toFixed(2)} L
             </li>}
                 {equipment.lauterDeadspace > 0 &&
-                  <li>
+                  <li title="Amount lost to the lauter tun and equipment associated with the lautering process">
                     <b>Lauter Deadspace: </b>{equipment.lauterDeadspace.toFixed(2)} L
             </li>}
                 {equipment.topUpKettle > 0 &&
-                  <li><b>TopUpKettle: </b>{equipment.topUpKettle.toFixed(2)} L</li>}
+                  <li title="Amount normally added to the boil kettle before the boil"><b>TopUpKettle: </b>{equipment.topUpKettle.toFixed(2)} L</li>}
                 {equipment.BIAB === true &&
-                  <li><b>BIAB: </b>{equipment.BIAB.toString()}</li>}
+                  <li title=""><b>BIAB: </b>{equipment.BIAB.toString()}</li>}
 
               </ul>
             )}
