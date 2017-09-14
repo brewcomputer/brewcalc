1b77663530231a547b6c73c77ef4603361123dbe
Yuriy Krutilin <y.krutilin@gmail.com>
2017-09-14 15:50:49 +0300

    kg to oz and step to simplify the stuff.

--- app/src/components/CrossUnitsInput.js
+++ app/src/components/CrossUnitsInput.js
@@ -2,7 +2,8 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 import {
-  litersToGallons
+  litersToGallons,
+  kgToOunces
 } from 'brewcalc'
 
 
@@ -11,6 +12,8 @@ const convertionMapper = (value, unit, units) => {
   switch (unit) {
     case 'L':
       return units !== 'metric' ? { value: (litersToGallons(value).toFixed(2)), unit: 'gal' } : { value: value, unit: unit }
+    case 'kg':
+      return units !== 'metric' ? { value: (kgToOunces(value).toFixed(2)), unit: 'oz' } : { value: value, unit: unit }
     default:
       return { value: value, unit: unit }
   }
@@ -20,7 +23,7 @@ const CrossUnitsInput = ({ name, description, value, unit, units }) => {
   const converted = convertionMapper(value, unit, units)
   return (
     (value !== '0.00' && value !== 'false') &&
-    <li title={description}><b>{name}: </b>{converted.value} {converted.unit}</li>
+    <div title={description}><b>{name} </b>{converted.value} {converted.unit}</div>
   )
 }
 
