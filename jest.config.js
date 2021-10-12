c31b0847c640e351c11797b843605f0fcfb9befd
alexlavrov <alex.s.lavrov@gmail.com>
2021-10-12 16:37:03 +0300

    Use npm, update react-scripts, fix workflow

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 module.exports = {
   rootDir: process.cwd(),
+  roots: ["<rootDir>/tests"],
   testEnvironment: "node",
   transform: {
     "^.+\\.(t|j)s$": path.resolve(__dirname, "jest.transform.js"),
