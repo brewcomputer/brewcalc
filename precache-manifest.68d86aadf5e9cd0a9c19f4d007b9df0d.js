f1bf9a6c96013453fca3571154b7934c79783fcf
alexslavr <alexslavr@users.noreply.github.com>
2021-10-13 08:51:54 +0000

    Deploying to gh-pages from  @ fa1bf7bd63b5fcc6301aca288cdff25b7a5affef 🚀

---
+++ precache-manifest.68d86aadf5e9cd0a9c19f4d007b9df0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2309b3f409c21eb1788ff4801e74e5e0",
+    "url": "/brewcalc/index.html"
+  },
+  {
+    "revision": "3a6c179d8e0d1e98a3ba",
+    "url": "/brewcalc/static/css/main.801c87dc.chunk.css"
+  },
+  {
+    "revision": "f128d5f134389145f784",
+    "url": "/brewcalc/static/js/2.c51897cb.chunk.js"
+  },
+  {
+    "revision": "5ed92eff82e22231cb96ec7cb280983a",
+    "url": "/brewcalc/static/js/2.c51897cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a6c179d8e0d1e98a3ba",
+    "url": "/brewcalc/static/js/main.ca919db9.chunk.js"
+  },
+  {
+    "revision": "a26396c003b4ecf102af",
+    "url": "/brewcalc/static/js/runtime-main.27e8bc2d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/brewcalc/precache-manifest.15407713c249b569652c143ccc146d35.js"
+  "/brewcalc/precache-manifest.68d86aadf5e9cd0a9c19f4d007b9df0d.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/brewcalc/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/brewcalc/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
